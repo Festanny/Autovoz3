d5105c5e76684ea5e979b202feb0b7048a10ae10
Festanny <60522731+Festanny@users.noreply.github.com>
2022-10-05 00:03:46 +0400

    upd

--- js/main.js
+++ js/main.js
@@ -79,6 +79,22 @@
     });
     });
 
+    // Просмотр изображения
+	$(function(){
+		$('.minimized').click(function(event) {
+		var i_path = $(this).attr('src');
+		$('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+		$('#overlay, #magnify').fadeIn('fast');
+		});
+		
+		$('body').on('click', '#close-popup, #overlay', function(event) {
+		event.preventDefault();
+		$('#overlay, #magnify').fadeOut('fast', function() {
+			$('#close-popup, #magnify, #overlay').remove();
+		});
+		});
+	});
+
 
     var $document = $(document),
         $window = $(window),
